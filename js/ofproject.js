045dbe9a98547b3963b18e08d96b90d01039769e
hyunao <89890370+hyuna323@users.noreply.github.com>
2023-04-19 04:01:56 +0900

    viewport 슬라이드 기능, 폰트 크기 전체 수정

--- js/ofproject.js
+++ js/ofproject.js
@@ -1,10 +1,25 @@
-$(function(){
+(function(){
 
-  $('#main_page').fullpage({
+  ('#main_page').fullpage({
 		//options here
     anchors: ['viewport', 'about', 'place', 'contact'],
 		autoScrolling:true,
     navigation:true,
     navigationPosition:'right'
 	});
+});
+
+// viewBox ==============================
+
+const swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 });
